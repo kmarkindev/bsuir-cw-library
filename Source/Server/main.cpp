c121dafe5675a11b4b4f09a310684188c55bd1c7
Kirill Markin <kmarkindev@gmail.com>
2022-05-12 13:48:51 +0300

    Revert config loading

--- Source/Server/main.cpp
+++ Source/Server/main.cpp
@@ -1,20 +1,8 @@
 #include <drogon/drogon.h>
-#include <filesystem>
-#include <fstream>
 
-Json::Value loadConfigFile()
+int main()
 {
-    Json::Value config;
-
-    auto path = std::filesystem::path("./configs/config.json");
-    std::ifstream fs(path);
-    fs >> config;
-
-    return config;
-}
-
-int main() {
     drogon::app()
-        .loadConfigJson(loadConfigFile())
+        .loadConfigJson("configs/config.json")
         .run();
 }
