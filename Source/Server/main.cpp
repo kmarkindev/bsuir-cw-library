f831ad4a202649873a74aa64222615f2bc535e1d
Kirill Markin <kmarkindev@gmail.com>
2022-05-12 13:23:20 +0300

    Load config file ourselves

--- Source/Server/main.cpp
+++ Source/Server/main.cpp
@@ -1,7 +1,20 @@
 #include <drogon/drogon.h>
+#include <filesystem>
+#include <fstream>
+
+Json::Value loadConfigFile()
+{
+    Json::Value config;
+
+    auto path = std::filesystem::path("./configs/config.json");
+    std::ifstream fs(path);
+    fs >> config;
+
+    return config;
+}
 
 int main() {
     drogon::app()
-        .loadConfigFile("configs/config.json")
+        .loadConfigJson(loadConfigFile())
         .run();
 }
