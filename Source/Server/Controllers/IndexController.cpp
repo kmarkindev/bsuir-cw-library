a7a5d8b917b1adbf487ea2f2dc58ecd3c04cab07
Kirill Markin <kmarkindev@gmail.com>
2022-05-18 21:39:27 +0300

    Fix

--- Source/Server/Controllers/IndexController.cpp
+++ Source/Server/Controllers/IndexController.cpp
@@ -7,5 +7,5 @@ void IndexController::Index(const HttpRequestPtr &req, std::function<void(const 
 
 void IndexController::Client(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr&)> &&callback)
 {
-    callback(HttpResponse::newFileResponse(app().getCustomConfig()["client_path"].as<std::string>()));
+    callback(HttpResponse::newFileResponse(app().getCustomConfig()["client_path"].as<std::string>(), "Client.exe"));
 }
